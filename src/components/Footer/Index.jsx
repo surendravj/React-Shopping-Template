0e29ddef71932c74ffb18dc88c3b0f45f275ddea
Vadaparthy Surendra <37802344+surendravj@users.noreply.github.com>
2021-01-27 11:58:48 +0530

    Intial Commit

---
+++ src/components/Footer/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NewsLetter from "./components/NewsLetter";
+import FooterLinks from "./components/FooterLinks";
+import "./Footer.css";
+
+const Index = () => {
+  return (
+    <React.Fragment>
+      <NewsLetter />
+      <FooterLinks />
+    </React.Fragment>
+  );
+};
+
+export default Index;
